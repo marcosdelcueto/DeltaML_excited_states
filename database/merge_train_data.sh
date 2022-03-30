ec5a3046f8241fce715972df1c9138dc3b259696
Marcos del Cueto <57792888+marcosdelcueto@users.noreply.github.com>
2022-03-30 18:23:00 +0100

    Update merge_train_data.sh

--- database/merge_train_data.sh
+++ database/merge_train_data.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-rm -f train_data.csv
-for i in `seq -w 01 05`
-do 
-    unzip -o train_data_${i}.zip
-    cat train_data_${i}.csv >> train_data.csv
-    rm -f train_data_${i}.csv
-done
-
+# if train_data.csv does not exist, loop through each zip file
+if [[ ! -f 'train_data.csv' ]]
+then
+    for i in `seq -w 01 05`
+    do  
+        unzip -o train_data_${i}.zip                  # unzip each part
+        cat train_data_${i}.csv >> train_data.csv     # concatenate each part into train_data.csv
+        rm -f train_data_${i}.csv                     # remove intermediate csv files
+        rm -f train_data_${i}.zip                     # remove zip files
+    done
+fi  
